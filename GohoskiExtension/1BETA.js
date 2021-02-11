3754f28cce7064aaf5e0a42dba0e8ad049550681
Глеб (Gleb) <appdata.youtube@bk.ru>
2021-02-11 11:30:41 +0300

    Update 1BETA.js

--- GohoskiExtension/1BETA.js
+++ GohoskiExtension/1BETA.js
@@ -1,4 +1,3 @@
-/*eslint-env browser*/
 class GohoskiExtension {
 	getInfo() {
 		return {
@@ -6,13 +5,6 @@ class GohoskiExtension {
 			"name": "Gohoski's Extension",
 			"blocks": [
 				{
-					"opcode": "screenshot", //This will be the ID code for the block
-					"blockType": "command", //This can either be Boolean, reporter, command, or hat
-					"text": "screenshot the scene and save it", //This is the block text, and how it will display in the Scratch interface
-					"arguments": { //Arguments are the input fields in the block. In the block text, place arguments in square brackets with the corresponding ID 
-					}
-				},
-				/* {
 					"opcode": "playSoundFromUrl", 
 					"blockType": "command",
 					"text": "play sound [url] from url", 
@@ -22,16 +14,36 @@ class GohoskiExtension {
 							"defaultValue": "scene"
 						}
 					}
-				}, */
+				},
 			],
 			"menus": {
 				
 			}
 		};
 	}
-	screenshot({}) {
-		var canvas = document.querySelector('canvas[style="height: 306px; width: 408px;"]');
-        window.open(canvas.toDataURL("image/png"));
+	playSoundFromUrl({url}) {
+		// 1. Создаём новый XMLHttpRequest-объект
+		let xhr = new XMLHttpRequest();
+
+		// 2. Настраиваем его: GET-запрос по URL /article/.../load
+		xhr.open('GET', url);
+
+		// 3. Отсылаем запрос
+		xhr.send();
+
+		// 4. Этот код сработает после того, как мы получим ответ сервера
+		xhr.onload = function() {
+		  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+		    	alert(`Error: xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+		  } else { // если всё прошло гладко, выводим результат
+		    	var audio = new Audio(xhr.response);
+			audio.play();
+		  }
+		};
+
+		xhr.onerror = function() {
+		  	alert("Request failed");
+		};
 	}
 }
 Scratch.extensions.register(new GohoskiExtension());
