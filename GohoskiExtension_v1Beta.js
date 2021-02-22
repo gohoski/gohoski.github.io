8a825d834857b37b1be8d7125590e25c0f6b45b5
GithubBuyancy <vyacheslav.tikhomirov.2021@mail.ru>
2021-02-22 21:11:14 +0300

    Update GohoskiExtension_v1Beta.js

--- GohoskiExtension_v1Beta.js
+++ GohoskiExtension_v1Beta.js
@@ -22,7 +22,7 @@ class GohoskiExtension {
 	}
 	data_http({url}) {
 		let xhr = new XMLHttpRequest();
-		xhr.open('GET', '/article/xmlhttprequest/example/load');
+		xhr.open('GET', url);
 		xhr.send();
 		xhr.onload = function() {
 		  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
