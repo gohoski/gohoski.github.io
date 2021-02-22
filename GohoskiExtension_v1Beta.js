8a00bc942e22c5d777dcbc682b35030bffbf54e7
GithubBuyancy <vyacheslav.tikhomirov.2021@mail.ru>
2021-02-22 21:06:30 +0300

    Update GohoskiExtension_v1Beta.js

--- GohoskiExtension_v1Beta.js
+++ GohoskiExtension_v1Beta.js
@@ -5,33 +5,31 @@ class GohoskiExtension {
 			"name": "Gohoski's Extension",
 			"blocks": [
 				{
-					"opcode": "screenshot_scene", //This will be the ID code for the block
-					"blockType": "command", //This can either be Boolean, reporter, command, or hat
-					"text": "save the scene as png", //This is the block text, and how it will display in the Scratch interface
+					"opcode": "data_http", //This will be the ID code for the block
+					"blockType": "reporter", //This can either be Boolean, reporter, command, or hat
+					"text": "data from url: [url]", //This is the block text, and how it will display in the Scratch interface
 					"arguments": { //Arguments are the input fields in the block. In the block text, place arguments in square brackets with the corresponding ID
 					}
-				},
-				{
-					"opcode": "open_link_in_new_tab", 
-					"blockType": "command",
-					"text": "open url [link] in a new tab",
-					"arguments": {
-						"url": {
-							"type": "string",
-							"defaultValue": "scratch.mit.edu"
-						}
-					}
-				},
+				}
 			],
 			"menus": { //we will get back to this in a later tutorial
 			}
 		};
 	}
-	screenshot_scene({}) {
-		window.open(document.getElementsByTagName("canvas")[0].toDataURL());
-	}
-	open_link_in_new_tab({link}) {
-		window.open(link,'_blank');
+	data_http({url}) {
+		let xhr = new XMLHttpRequest();
+		xhr.open('GET', '/article/xmlhttprequest/example/load');
+		xhr.send();
+		xhr.onload = function() {
+		  if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+			console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+		  } else { // если всё прошло гладко, выводим результат
+			return xhr.response;
+		  }
+		};
+		xhr.onerror = function() {
+		  console.log("Запрос не удался");
+		};
 	}
 }
 Scratch.extensions.register(new GohoskiExtension());
